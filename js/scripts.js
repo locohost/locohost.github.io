119312a3929a400099fef198e3f97ce484f2b603
locohost <locohost@gmail.com>
2019-07-22 10:07:05 -0400

    - Updated help text, worked on TRYING to get div content to NOT overlap footer. CANT do it?!?!

--- js/scripts.js
+++ js/scripts.js
@@ -90,23 +90,3 @@ $('main').find('.card, .btn-group, .btn, .navbar, .modal, .form-group, .list-gro
 		return false;
 	});
 });
-
-function loadTemplate(templateName, domSelector) {
-	templateName = templateName.indexOf('/template/') == 0 ? templateName : '/template/' + templateName;
-	templateName += templateName.indexOf('.html') > 0 ? '' : '.html';
-	console.log('templateName: ', templateName);
-	$.ajax({
-		url: templateName,
-		context: $(domSelector)
-	}).done(function (data) {
-		$(this).empty().append(data);
-	});
-}
-
-function loadSubPageTemplate(templateName) {
-	loadTemplate(templateName, 'div.page-content');
-}
-
-function loadIndexPageTemplate(templateName) {
-	loadTemplate(templateName, 'div.page-container');
-}
